42bca36ce1bf5e856d28498d4fb7664e6077bd84
viikkkas <61241966+viikkkas@users.noreply.github.com>
2020-08-13 16:04:27 +0530

    Create AccessingLibrary.js

---
+++ AccessingLibrary.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Screen from "./app/components/Screen";
+import { Button } from "react-native";
+
+function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!granted) {
+      alert("You need to enable permission to access the library");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("Error picking an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
+}
+
+export default App;
